0d7817ed687f18792760854046d0c9aa341e357a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-05-18 15:02:41 +0800

    keepass imported

--- src/error.rs
+++ src/error.rs
@@ -2,4 +2,15 @@
 pub enum Error {
     #[error("eframe::Error: {0}")]
     Eframe(#[from] eframe::Error),
+
+    #[error("keepass::error::DatabaseOpenError: {0}")]
+    KeepassDatabaseOpenError(#[from] keepass::error::DatabaseOpenError),
+
+    #[error("std::io::Error: {0}")]
+    StdIoError(#[from] std::io::Error),
+
+    #[error("dotenvy::Error: {0}")]
+    DotenvyError(#[from] dotenvy::Error),
 }
+
+pub type Result<T> = std::result::Result<T, Error>;

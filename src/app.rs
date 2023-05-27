03c719f846d6acc659341b00b2e4ecfc6b468351
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-05-27 23:25:44 +0800

    refine keepass

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,56 @@
-use eframe::egui;
+use eframe::egui::{self, ScrollArea};
+
+const WHITE: egui::Color32 = egui::Color32::from_rgb(255, 255, 255);
+const PADDING: f32 = 1.0;
 
 #[derive(Default)]
-pub struct App {}
+pub struct App {
+    kp_items: Vec<KpItem>,
+}
 
 impl App {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+        let iter = (0..20).map(|i| KpItem {
+            title: format!("title {}", i),
+            username: format!("username {}", i),
+            password: format!("password {}", i),
+        });
+        Self {
+            kp_items: iter.collect(),
+        }
+    }
+
+    fn render_kp_items(&mut self, ui: &mut egui::Ui) {
+        for item in &mut self.kp_items {
+            ui.separator();
+            ui.add_space(PADDING);
+            ui.colored_label(WHITE, item.title.clone());
+            ui.add_space(PADDING);
+            ui.horizontal(|ui| {
+                ui.label(item.username.clone());
+                ui.label(item.password.clone());
+            });
+            ui.add_space(PADDING);
+        }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+            ui.heading("KeePass items");
+            ui.label("This is a list of KeePass items");
+
+            ScrollArea::vertical().show(ui, |ui| {
+                self.render_kp_items(ui);
+            });
         });
     }
+}
+
+#[derive(Debug, Default)]
+struct KpItem {
+    title: String,
+    username: String,
+    password: String,
 }

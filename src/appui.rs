b3aee4836adb25beca698508448e7debd1389396
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-06-08 01:52:13 +0800

    find_cjk_fonts

--- src/appui.rs
+++ src/appui.rs
@@ -1,5 +1,6 @@
 use crate::{
     error::Error,
+    fonts::find_cjk_fonts,
     keepass::KpDb,
     password,
     uistate::{Config, UiState},
@@ -47,53 +48,25 @@ impl AppUI {
     }
 
     pub fn configure_fonts(ctx: &eframe::egui::Context) -> Option<()> {
-        let font_file = Self::find_cjk_font()?;
-        let font_name = font_file.split('/').last()?.split('.').next()?.to_string();
-        let font_file_bytes = std::fs::read(font_file).ok()?;
-
-        let font_data = eframe::egui::FontData::from_owned(font_file_bytes);
         let mut font_def = eframe::egui::FontDefinitions::default();
-        font_def.font_data.insert(font_name.to_string(), font_data);
 
-        let font_family = eframe::epaint::FontFamily::Proportional;
-        font_def.families.get_mut(&font_family)?.insert(0, font_name);
+        let font_files = find_cjk_fonts()?;
+        for font_file in font_files.iter() {
+            Self::add_font(&mut font_def, font_file);
+        }
 
         ctx.set_fonts(font_def);
         Some(())
     }
 
-    fn find_cjk_font() -> Option<String> {
-        #[cfg(unix)]
-        {
-            use std::process::Command;
-            // linux/macOS command: fc-list
-            let output = Command::new("sh").arg("-c").arg("fc-list").output().ok()?;
-            let stdout = std::str::from_utf8(&output.stdout).ok()?;
-            #[cfg(target_os = "macos")]
-            let font_line = stdout
-                .lines()
-                .find(|line| line.contains("Regular") && line.contains("Hiragino Sans GB"))
-                .unwrap_or("/System/Library/Fonts/Hiragino Sans GB.ttc");
-            #[cfg(target_os = "linux")]
-            let font_line = stdout
-                .lines()
-                .find(|line| line.contains("Regular") && line.contains("CJK"))
-                .unwrap_or("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc");
-
-            let font_path = font_line.split(':').next()?.trim();
-            Some(font_path.to_string())
-        }
-        #[cfg(windows)]
-        {
-            let font_file = {
-                // c:/Windows/Fonts/msyh.ttc
-                let mut font_path = PathBuf::from(std::env::var("SystemRoot").ok()?);
-                font_path.push("Fonts");
-                font_path.push("msyh.ttc");
-                font_path.to_str()?.to_string().replace("\\", "/")
-            };
-            Some(font_file)
-        }
+    fn add_font(font_def: &mut eframe::egui::FontDefinitions, font_file: &std::path::PathBuf) -> Option<()> {
+        let font_name = font_file.file_stem()?.to_str()?.to_string();
+        let font_file_bytes = std::fs::read(font_file).ok()?;
+        let font_data = eframe::egui::FontData::from_owned(font_file_bytes);
+        font_def.font_data.insert(font_name.to_string(), font_data);
+        let font_family = eframe::epaint::FontFamily::Proportional;
+        font_def.families.get_mut(&font_family)?.insert(0, font_name);
+        Some(())
     }
 
     fn render_kp_items(&mut self, ui: &mut egui::Ui) {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod appui;
 pub mod error;
+pub mod fonts;
 pub mod keepass;
 pub mod password;
 pub mod uistate;

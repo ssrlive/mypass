547f2a9f967e9556461aba420784e531f79903f7
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-06-07 10:52:03 +0800

    refactor font logic

--- src/appui.rs
+++ src/appui.rs
@@ -1,8 +1,7 @@
 use crate::{error::Error, keepass::KpDb, password};
 use eframe::{
-    egui::{self, Context, FontData, FontDefinitions, Hyperlink, Label, RichText, ScrollArea, TopBottomPanel},
+    egui::{self, Hyperlink, Label, RichText, ScrollArea, TopBottomPanel},
     emath::Align,
-    epaint::FontFamily,
 };
 use keepass::db::NodeRef;
 use std::path::PathBuf;
@@ -110,61 +109,54 @@ impl AppUI {
         }
     }
 
-    pub fn configure_fonts(ctx: &Context) -> Option<()> {
-        #[cfg(unix)]
-        let font_file = Self::unix_find_cjk_font()?;
-
-        #[cfg(windows)]
-        let font_file = {
-            let mut font_path = PathBuf::from(std::env::var("SystemRoot").ok()?);
-            font_path.push("Fonts");
-            font_path.push("msyh.ttc");
-            font_path.to_str()?.to_string()
-        };
-
-        let font_name = Self::get_font_name(&font_file)?;
-
+    pub fn configure_fonts(ctx: &eframe::egui::Context) -> Option<()> {
+        let font_file = Self::find_cjk_font()?;
+        let font_name = font_file.split('/').last()?.split('.').next()?.to_string();
         let font_file_bytes = std::fs::read(font_file).ok()?;
 
-        let mut font_def = FontDefinitions::default();
-        font_def
-            .font_data
-            .insert(font_name.to_string(), FontData::from_owned(font_file_bytes));
+        let font_data = eframe::egui::FontData::from_owned(font_file_bytes);
+        let mut font_def = eframe::egui::FontDefinitions::default();
+        font_def.font_data.insert(font_name.to_string(), font_data);
 
-        font_def
-            .families
-            .get_mut(&FontFamily::Proportional)
-            .unwrap()
-            .insert(0, font_name.to_string());
+        let font_family = eframe::epaint::FontFamily::Proportional;
+        font_def.families.get_mut(&font_family)?.insert(0, font_name);
 
         ctx.set_fonts(font_def);
         Some(())
     }
 
-    #[cfg(unix)]
-    fn unix_find_cjk_font() -> Option<String> {
-        use std::process::Command;
-        // linux/macOS command: fc-list
-        let output = Command::new("sh").arg("-c").arg("fc-list").output().ok()?;
-        let stdout = std::str::from_utf8(&output.stdout).ok()?;
-        #[cfg(target_os = "macos")]
-        let font_line = stdout
-            .lines()
-            .find(|line| line.contains("Regular") && line.contains("Hiragino Sans GB"))
-            .unwrap_or("/System/Library/Fonts/Hiragino Sans GB.ttc");
-        #[cfg(target_os = "linux")]
-        let font_line = stdout
-            .lines()
-            .find(|line| line.contains("Regular") && line.contains("CJK"))
-            .unwrap_or("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc");
+    fn find_cjk_font() -> Option<String> {
+        #[cfg(unix)]
+        {
+            use std::process::Command;
+            // linux/macOS command: fc-list
+            let output = Command::new("sh").arg("-c").arg("fc-list").output().ok()?;
+            let stdout = std::str::from_utf8(&output.stdout).ok()?;
+            #[cfg(target_os = "macos")]
+            let font_line = stdout
+                .lines()
+                .find(|line| line.contains("Regular") && line.contains("Hiragino Sans GB"))
+                .unwrap_or("/System/Library/Fonts/Hiragino Sans GB.ttc");
+            #[cfg(target_os = "linux")]
+            let font_line = stdout
+                .lines()
+                .find(|line| line.contains("Regular") && line.contains("CJK"))
+                .unwrap_or("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc");
 
-        let font_path = font_line.split(':').next()?.trim();
-        Some(font_path.to_string())
-    }
-
-    fn get_font_name(font_path: &str) -> Option<String> {
-        let font_name = font_path.split('/').last()?.split('.').next()?;
-        Some(font_name.to_string())
+            let font_path = font_line.split(':').next()?.trim();
+            Some(font_path.to_string())
+        }
+        #[cfg(windows)]
+        {
+            let font_file = {
+                // c:/Windows/Fonts/msyh.ttc
+                let mut font_path = PathBuf::from(std::env::var("SystemRoot").ok()?);
+                font_path.push("Fonts");
+                font_path.push("msyh.ttc");
+                font_path.to_str()?.to_string().replace("\\", "/")
+            };
+            Some(font_file)
+        }
     }
 
     fn render_kp_items(&mut self, ui: &mut egui::Ui) {
